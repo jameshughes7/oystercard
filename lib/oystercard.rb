211c04e9ba9728c6dda163233de9f4b3d21893ac
James Hughes <hugjimbo77@gmail.com>
2017-09-05 22:14:20 +0100

    touch in/out & minimum balance methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
-
+  attr_reader :balance
 MAXIMUM_BALANCE = 20
 
 attr_reader :balance
 
-  def initialize
-    @balance = 0
-  end
+def initialize
+  @balance = 0
+  @in_journey = false
+end
 
   def top_up(amount)
     fail "Error: maximum balance of 20 exceeded" if amount + balance > MAXIMUM_BALANCE
@@ -17,7 +18,25 @@ attr_reader :balance
     @balance -= amount
   end
 
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def touch_in
+    fail "Insufficient balance to touch in" if balance < 1
+    @in_journey = true
+  end
+
 end
+
+#  def touch_in
+#    fail 'Cannot touch in: minimum balance not met' if balance < CARD_MIN_BALANCE
+#    @in_journey = true
+#  end
 
 
 =begin (attr_reader)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,40 +1,54 @@
 require 'oystercard'
-
 RSpec.describe Oystercard do
-#subject {described_class.new(balance: 0) }
+  subject(:oystercard) { described_class.new }
 
     it { is_expected.to(respond_to(:top_up).with(1).argument) }
     it { is_expected.to(respond_to(:deduct).with(1).argument) }
 
-  describe '#starting_balance' do
+  context 'Balance' do
     it "the balance is zero" do
-      expect(subject.balance).to eq(0)
+      expect(oystercard.balance).to eq(0)
     end
-  end
 
-  describe '#top_up' do
     it "can top up the balance" do
-      expect{ subject.top_up (1) }.to change{ subject.balance }.by (1)
+      expect{ oystercard.top_up (1) }.to change{ oystercard.balance }.by (1)
     end
 
     it "raises an error if the maximum balance is exceeded" do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up(1) }.to raise_exception "Error: maximum balance of #{maximum_balance} exceeded"
+      oystercard.top_up(maximum_balance)
+      expect{ oystercard.top_up(1) }.to raise_exception "Error: maximum balance of #{maximum_balance} exceeded"
     end
-  end
 
-  describe '#deduct_fare' do
     it "can deduct money from the balance" do
-      expect{ subject.deduct (1) }.to change{ subject.balance }.by (-1)
+      expect{ oystercard.deduct (1) }.to change{ oystercard.balance }.by (-1)
     end
   end
 
+  context 'journey' do
+    it 'is initially not in a journey' do
+      expect(oystercard).not_to be_in_journey
+    end
+
+    it 'can touch in' do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+
+    it 'can touch out' do
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
+
+    it 'will not touch in if below minimum balance' do
+      expect{ oystercard.touch_in }.to raise_error "Insufficient balance to touch in"
+    end
+  end
 end
-
-
-
-
-  #it "add money to my card" do
-  #  expect(subject.top_up).to change(subject, :balance).by value
-  #end
+=begin
+   it 'raises an error if card does not have minimum balance' do
+      card_min_balance = Oystercard::CARD_LIMIT
+      oystercard.touch_in
+      expect{ oystercard.touch_in}.to raise_error 'Cannot touch in: minimum balance not met'
+    end
+=end
